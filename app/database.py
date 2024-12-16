a286c854bc38c9a13a6ac2685834e17e39d848ea
koul1n <na1zyotb@gmail.com>
2024-12-16 19:19:11 +0300

    async sessionmaker

--- app/database.py
+++ app/database.py
@@ -1,26 +1,20 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@127.0.0.1/postgres"
 
-
-
+# Создаем асинхронный движок
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-
-async_session = sessionmaker(
+# Создаем фабрику сессий
+async_session = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
-    expire_on_commit=False,
-    autocommit=False,
+    expire_on_commit=False
 )
 
-
-
+# Асинхронная функция для получения сессии
 async def get_db():
     async with async_session() as session:
         yield session
-
-
-
 
 
