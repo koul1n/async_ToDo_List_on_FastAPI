190e475ea14d02d157d72219eb1ff39d6e3adba7
koul1n <na1zyotb@gmail.com>
2025-01-07 11:14:47 +0300

    change readme file

--- app/security/config.py
+++ app/security/config.py
@@ -1,9 +1,13 @@
 from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class AuthSettings(BaseSettings):
-    SECRET_KEY: str = "SECRET_KEY"
-    ALGORITHM: str = "HS256"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
     expires_in: int = 15
 
 auth_settings = AuthSettings()
