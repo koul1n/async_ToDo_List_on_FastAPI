ef73bd37270267329c5a69e14d0a398ef62ea981
koul1n <na1zyotb@gmail.com>
2025-01-17 17:01:59 +0300

    add env example file

--- app/users/crud.py
+++ app/users/crud.py
@@ -9,6 +9,25 @@ from app.security import hash_password
 
 
 async def get_user(*, db : AsyncSession, email : EmailStr = None, user_id : int = None, username : str = None):
+    """
+       Получение пользователя из базы данных.
+
+       Эта функция позволяет получить пользователя по одному из переданных параметров:
+       email, user_id или username. Если ни один параметр не указан, возвращается None.
+
+       Параметры:
+       - db (AsyncSession): Асинхронная сессия базы данных.
+       - email (EmailStr, optional): Email пользователя. Если указан, поиск производится по email.
+       - user_id (int, optional): Идентификатор пользователя. Если указан, поиск производится по user_id.
+       - username (str, optional): Имя пользователя. Если указано, поиск производится по username.
+
+       Возвращает:
+       - User: Найденный пользователь или None, если пользователь не найден или параметры не указаны.
+
+       Примечание:
+       - Если указано несколько параметров (email, user_id, username), используется только первый из них.
+       - При отсутствии всех параметров возвращается None.
+    """
     if email:
         result = await db.execute(select(User).filter(User.email == email))
         return result.scalars().first()
